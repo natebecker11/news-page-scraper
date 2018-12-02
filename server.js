9456abd3eca31e6ec48f15a861496baf1926b117
natebecker11 <natebecker11@gmail.com>
2018-12-02 18:06:06 -0500

    express server setup

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
-const express = require('express')
+const app = require('express')
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.static('./dist'))
+
+const path = require('path')
 const mongoose = require('mongoose')
 const axios = require('axios')
 const cheerio = require('cheerio')
@@ -17,3 +24,11 @@ const db = mongoose.connection
 db.on('error', err => console.error(`Mongoose Error: ${err}`))
 
 db.once('open', _ => console.log('Mongoose connection successful.'))
+
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'))
+})
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT);
