903a068436d5e85ee7461dcc994446eebb928cdf
natebecker11 <natebecker11@gmail.com>
2018-12-03 16:04:26 -0500

    front-end display articles

---
+++ src/components/ArticleCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <a :href='this.article.link'> <h3> {{ article.title }} </h3> </a>
+    <p>By {{ article.author }} </p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['article']
+}
+</script>
+
---
+++ src/views/Content.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class='main-grid'>
+    <section class='header'>
+      <h1>News Page Scraper</h1>
+      <h2>All The News That's Fit To Scrape</h2>
+    </section>
+    <section class="articles">
+      <!-- <div v-for='(article, i) in articles' :key='i'>
+        <h3> {{ article.title }} </h3>
+      </div> -->
+      <article-card  v-for='(article, i) in articles' :key='i' :article='article'></article-card>
+
+    </section>
+    <section class="comments">
+
+    </section>
+    <footer class="footer">
+      <p>Copyright 2018 <a href='http://natebecker11.com' target='_blank'>Nate Becker</a></p>
+    </footer>
+  </div>
+</template>
+
+
+<script>
+import articleCard from '../components/ArticleCard'
+export default {
+  computed: {
+    articles() {
+      return this.$store.getters.getCurrentArticles
+    },
+    comments() {
+      return this.$store.getters.getCurrentComments
+    },
+    activeArticle() {
+      return this.$store.getters.getActiveArticleId
+    }
+  },
+  components: {
+    articleCard
+  }
+}
+</script>
+
+
+
+<style scoped>
+
+  .main-grid {
+    display: grid;
+    grid-template-columns: 5fr 2fr; 
+    grid-gap: '20px';
+    grid-template-areas: 
+      'header header'
+      'articles addcomment'
+      'articles comments'
+      'articles comments'
+      'footer footer';
+  }
+
+  .header {
+    grid-area: header;
+  }
+
+  .articles {
+    grid-area: articles;
+  }
+
+  .comments {
+    grid-area: comments;
+  }
+
+  .addcomment {
+    grid-area: addcomment;
+  }
+
+  .footer {
+    grid-area: footer;
+  }
+</style>
