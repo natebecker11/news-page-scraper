798a34f48e8ea5816858d6d0d7d8adc33c8a8112
natebecker11 <natebecker11@gmail.com>
2018-12-02 22:00:57 -0500

    started building store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    currentArticles: [],
+    currentComments: [],
+    activeArticleId: ''
   },
   mutations: {
-
+    setCurrentArticles(state, articles) {
+      state.currentArticles = articles
+    },
+    setCurrentComments(state, comments) {
+      state.currentComments = comments
+    },
+    setActiveArticle(state, articleId) {
+      state.activeArticleId = articleId
+    }
   },
   actions: {
-
+    scrapeArticles() {
+      return axios.post('/api/scrape')
+        .then(({ scraped }) => {})
+    }
   }
 })
